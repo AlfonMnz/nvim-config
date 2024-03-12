a203d88470109fadc5af6b3b32e0cb50bd89649b
AlfonMnz-beonit <amunoz@beonit.es>
2024-03-12 08:18:37 +0100

    removing blank spaces

--- lua/poliedro/plugins/nvim-tree.lua
+++ lua/poliedro/plugins/nvim-tree.lua
@@ -3,12 +3,9 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local nvimtree = require("nvim-tree")
-  
       -- recommended settings from nvim-tree documentation
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
-  
-  
       -- configure nvim-tree
       nvimtree.setup({
         view = {
