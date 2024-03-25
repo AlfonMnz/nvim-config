fa6fa1b9de63598429251d1759ece5499cd67bfc
AlfonMnz <alfonmusan14@gmail.com>
2024-03-25 09:41:05 +0100

    stuffs

--- lua/poliedro/plugins/telescope.lua
+++ lua/poliedro/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	tag = '0.1.x',
+	tag = '0.1.6',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	defaults = {
 		file_ignore_patterns = { "node_modules", ".git/"}
